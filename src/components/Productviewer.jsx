ad796911d43d59c917db568e6e209d07ccc45b29
AlanAndray01 <syedarslanshah7861@gmail.com>
2025-10-20 23:19:07 +0500

    4th commit
    Added laptop component in Product viewer

--- src/components/Productviewer.jsx
+++ src/components/Productviewer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import clsx from 'clsx';
+import MacbookModel14 from './models/Macbook-14';
 import { Box } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import useWindowsStore from '../store/store'
@@ -33,7 +34,8 @@ const Productviewer = () => {
             </div>
 
             <Canvas id="canvas" camera={{position: [0,2,5], fov:50, near:0.1, far: 100}}>
-                <Box position={[0, 0, 0]} scale={10 * scale} material-color={color} />
+                <ambientLight intensity={9} /> 
+                <MacbookModel14 scale={0.06} position={[0,0,0]} />
                 <OrbitControls enableZoom={false}/>
             </Canvas >
 
