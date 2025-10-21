ef8734d8728601b8992209ef2a154197f98b6892
AlanAndray01 <syedarslanshah7861@gmail.com>
2025-10-21 11:19:42 +0500

    6th commit
    Added colors for laptop

--- src/components/Productviewer.jsx
+++ src/components/Productviewer.jsx
@@ -21,7 +21,7 @@ const Productviewer = () => {
         <section id="product-viewer">
             <h2>Take a closer look!</h2>
             <div className="controls">
-                <p className="info">MacbookPro {scale} in {color}</p>
+                <p className="info">MacbookPro Pro | Available in 14" & 16" in Space Gray & Dark colors</p>
                 <div className="flex-center gap-5 mt-5">
                     <div className="color-control">
                         <div onClick={() => setColor('#adb5bd')} className={clsx('bg-neutral-300', color === '#adb5bd' && 'active')} />
--- src/components/models/Macbook-16.jsx
+++ src/components/models/Macbook-16.jsx
@@ -10,11 +10,28 @@ Title: macbook pro M3 16 inch 2024
 
 import React from 'react'
 import { useGLTF, useTexture } from '@react-three/drei';
+import { noChangeParts } from '../../constants';
+import useWindowsStore from '../../store/store';
+import { useEffect } from 'react';
+import { Color } from 'three';
+
 
 export default function MacbookModel16(props) {
-  const { nodes, materials } = useGLTF('/models/macbook-16-transformed.glb')
+  const {color} = useWindowsStore();
 
-  const texture = useTexture('/screen.png')
+  const { nodes, materials, scene } = useGLTF('/models/macbook-16-transformed.glb');
+
+  const texture = useTexture('/screen.png');
+  
+    useEffect(() => {
+    scene.traverse((child) => {
+      if (child.isMesh) {
+        if (!noChangeParts.includes(child.name)) {
+          child.material.color = new Color(color);
+        }
+      }
+    });
+  }, [color]);
   return (
     <group {...props} dispose={null}>
       <mesh geometry={nodes.Object_10.geometry} material={materials.PaletteMaterial001} rotation={[Math.PI / 2, 0, 0]} />
